e7650b3117cba997ae01db39dc4286be339b2b88
milude77 <milude797@gmail.com>
2025-05-05 17:58:04 +0800

    feat:更新数据库连接方式

--- backend/getWeather.py
+++ backend/getWeather.py
@@ -1,5 +1,6 @@
 #获取目标地区的天气信息
 import os
+import traceback
 import requests
 import mysql.connector
 import json
@@ -7,13 +8,16 @@ import datetime
 from bs4 import BeautifulSoup
 from dotenv import load_dotenv
 
-load_dotenv()
+
+fixed_directory = "/Weather-forecast-and-GPT-chat-platform"
+
+env_path = os.path.join(fixed_directory,'config','mysql_config.env')
+load_dotenv(dotenv_path=env_path)
 
 db_host = os.getenv("DB_HOST")
 db_user = os.getenv("DB_USER")
 db_password = os.getenv("DB_PASSWORD")
 
-fixed_directory = "/Weather-forecast-and-GPT-chat-platform"
 
 os.makedirs(fixed_directory, exist_ok=True)
 
@@ -22,11 +26,13 @@ file_path = os.path.join(fixed_directory, "weather.json")
 #获取地区对应的网页码
 def storeWeatherData() -> list:
     conn = mysql.connector.connect(
-    host= db_host,          # MySQL 主机
-    user= db_user,      # 数据库用户名
-    password= db_password,  # 数据库密码
-    database="weather"   # 数据库名称
-)
+        host=db_host,
+        user=db_user,
+        password=db_password,
+        database='weather'
+    )
+    if conn.is_connected():
+        print("成功连接到 MySQL 数据库")
     cursor = conn.cursor()
     cursor.execute("""
     SELECT 
@@ -75,8 +81,8 @@ if __name__ == "__main__":
         try:
             weather_data = getWeatherData(webpage_code)
             weather_content[city_name] = weather_data
-            #print(f"{city_name} 成功获取")
-        except error as e:  
+            print(f"{city_name} 成功获取")
+        except Exception as e:  
             print(f"{city_name} 获取失败")
     with open(file_path, "w", encoding="utf-8") as f:
         now_time = datetime.datetime.now()
--- backend/test.py
+++ backend/test.py
@@ -1,10 +1,22 @@
 import os
+import traceback
 import requests
 import mysql.connector
 import json
+import datetime
 from bs4 import BeautifulSoup
 from dotenv import load_dotenv
-from time import sleep
 
+fixed_directory = "/Weather-forecast-and-GPT-chat-platform"
+file_path = os.path.join(fixed_directory, "weather.json")
+env_path = os.path.join(fixed_directory,'config','mysql_config.env')
+fixed_directory = "/Weather-forecast-and-GPT-chat-platform"
 
-print(current_dir = os.path.dirname(os.path.realpath(__file__)))
+env_path = os.path.join(fixed_directory,'config','mysql_config.env')
+load_dotenv(dotenv_path=env_path)
+
+db_host = os.getenv("DB_HOST")
+db_user = os.getenv("DB_USER")
+db_password = os.getenv("DB_PASSWORD")
+
+print(db_host, db_user, db_password)
